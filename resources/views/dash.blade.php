4a84e2965f328075952ca99b6909fd2c4181d549
Kennedy <kennedy.c.alves@gmail.com>
2021-09-02 15:27:06 -0300

    up project

---
+++ resources/views/dash.blade.php
@@ -0,0 +1,31 @@
+@extends('adminlte::page')
+
+@section('title', 'BRAVE')
+
+@section('content_header')
+    <h1 class="m-0 text-dark">Dashboard</h1>
+@stop
+
+@section('content')
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="small-box bg-green">
+                        <div class="inner">
+                            <h3>53<sup style="font-size: 20px">%</sup></h3>
+            
+                            <p>Exemple Rate</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-stats-bars"></i>
+                        </div>
+                        <a href="#" class="small-box-footer">
+                            More info <i class="fa fa-arrow-circle-right"></i>
+                        </a>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
